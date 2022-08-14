c68405ea41266974e2a3549099eab92750566c53
surya03c <surya03c@gmail.com>
2022-08-14 17:59:32 -0400

    fixed dimensionality issues, grayscaling, eigenvalue correction, test case

--- LK.py
+++ LK.py
@@ -9,12 +9,14 @@ def isValidEig(arr, bound):
     vals,vecs = np.linalg.eig(np.matmul(np.transpose(arr),arr))
     eig1 = vals[0]
     eig2 = vals[1]
+    print(str(eig1))
+    print(str(eig2))
 
     if (eig1 > bound and eig2 > bound):
         if (eig1 > eig2):
-            return (abs(eig2/eig1) < 0.5)
+            return (abs(eig2/eig1) > 0.5)
         elif (eig1 < eig2):
-            return (abs(eig1/eig2) < 0.5)
+            return (abs(eig1/eig2) > 0.5)
     return False
      
 
@@ -28,6 +30,13 @@ def lucas_kanade(image1, image2, row, col, eig_lim, window_size=20):
     #assuming we are dealing with 8 bit images but still want to normalize
     im1 = im.imread(image1)/255 
     im2 = im.imread(image2)/255
+
+    R, G, B = im1[:,:,0], im1[:,:,1], im1[:,:,2]
+    im1 = 0.2989 * R + 0.5870 * G + 0.1140 * B
+    r, g, b = im2[:,:,0], im2[:,:,1], im2[:,:,2]
+    im2 = 0.2989 * r + 0.5870 * g + 0.1140 * b
+
+    print(im1.shape)
 
     assert(im1.size == im2.size)
 
@@ -58,7 +67,18 @@ def lucas_kanade(image1, image2, row, col, eig_lim, window_size=20):
     assert(isInvertible(arr))
     assert(isValidEig(arr, eig_lim)) #FILL IN A BOUND VAL
 
+    print("compiled successfully")
+
     return np.matmul((np.matmul(np.linalg.inv(np.matmul(np.transpose(arr),arr)), np.transpose(arr))), out)
+
+
+def main():
+    print(str(lucas_kanade("I1.jpeg", "I2.jpeg", 75, 75, 0.1)))
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 
